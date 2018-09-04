576897831ec07722c6a80f200c782f2a77001ff4
Dimitri C <couwenberg@gmail.com>
2018-09-04 17:29:59 +1000

    how to select stuff with just input of 2 letters fe

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,11 @@ export const getUsers = username => async dispatch => {
     const url = `https://jsonplaceholder.typicode.com/users`;
     const response = await fetch(url);
     const responseBody = await response.json();
+
+    const result = responseBody.filter(l => l.name.charAt(0)===username)
+
+    debugger;
+
     dispatch(addUsers(responseBody));
   } catch (error) {
     console.error(error);
