90969e9d2f2f4f8924595ffc2d8ef908539ffa4b
Dimitri C <couwenberg@gmail.com>
2018-09-21 18:10:02 +1000

    update

--- src/actions/counterAction.js
+++ src/actions/counterAction.js
@@ -2,7 +2,6 @@ export const incrementCounter = () => ({
   type: 'INCREMENT'
 })
 
-
 export const receivedUsers = (data) => (
   {
     type: 'RECEIVEDUSERS',
@@ -11,29 +10,8 @@ export const receivedUsers = (data) => (
 )
 
 //thunk
-// export const getUsers = dispatch => {
-//   fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(response => response.json())
-//     .then(json => dispatch(receivedUsers(json)))
-// }
-
-export const getUsers = () => async dispatch => {
-  try {
-    const url = `https://jsonplaceholder.typicode.com/users`;
-    const response = await fetch(url);
-    const responseBody = await response.json();
-    dispatch(receivedUsers(responseBody));
-  } catch (error) {
-    console.error(error);
-   
-  }
-};
-
-
-
-// const fetchPosts = subreddit => dispatch => {
-//   dispatch(requestPosts(subreddit))
-//   return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-//     .then(response => response.json())
-//     .then(json => dispatch(receivePosts(subreddit, json)))
-// }
+export const getUsers = () => dispatch => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => dispatch(receivedUsers(json)))
+}
