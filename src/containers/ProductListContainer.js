cf1e64e2e6e7605d66e320148651865272ad0631
Dimitri C <couwenberg@gmail.com>
2018-09-21 10:18:31 +1000

    bindactioncreators method 1

--- src/containers/ProductListContainer.js
+++ src/containers/ProductListContainer.js
@@ -3,14 +3,17 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import {ProductList}     from '../components/ProductList'
 import {receivedUsers} from '../actions/counterAction'
+import { bindActionCreators } from 'redux'
 
 const blaat = (state) =>({
   products: state.products
 })
 
-const mapDispatchToProps = {
-  receivedUsers
-}
-
+const mapDispatchToProps = dispatch => bindActionCreators(
+  {
+    receivedUsers
+  },
+  dispatch,
+)
 
 export default connect(blaat,mapDispatchToProps)(ProductList)
